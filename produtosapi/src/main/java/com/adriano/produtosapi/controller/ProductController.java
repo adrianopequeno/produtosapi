64bb7e7756c5920670174c981b1daf55152ff6bb
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-03-10 14:58:19 -0300

    Persisting products in the database

--- produtosapi/src/main/java/com/adriano/produtosapi/controller/ProductController.java
+++ produtosapi/src/main/java/com/adriano/produtosapi/controller/ProductController.java
@@ -1,18 +1,32 @@
 package com.adriano.produtosapi.controller;
 
 import com.adriano.produtosapi.model.Product;
+import com.adriano.produtosapi.repository.ProductRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         System.out.println("Produto reecebido: " + product);
+
+        var id = UUID.randomUUID().toString();
+        product.setId(id);
+
+        productRepository.save(product);
         return product;
     }
 }
