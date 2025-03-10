eb9229efb2ff6c9287d7006f862df600b791bc27
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-03-10 13:03:45 -0300

    started projetc

---
+++ produtosapi/src/main/java/com/adriano/produtosapi/ApiDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.adriano.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeProdutosApplication.class, args);
+	}
+
+}
---
+++ produtosapi/src/test/java/com/adriano/produtosapi/ApiDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
